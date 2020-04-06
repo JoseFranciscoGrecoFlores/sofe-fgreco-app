6f78b8a0e31a93bd3cc4046fea03cac597d27018
Francisco Greco <a320610@uach.mx>
2020-04-05 23:25:44 -0600

    Implement pagination of countries table, fix css on world map

--- ember-quickstart/app/components/countries-list.js
+++ ember-quickstart/app/components/countries-list.js
@@ -3,17 +3,52 @@ import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
 
 export default class CountriesListComponent extends Component {
-  @tracked sortedList;
+  @tracked listToDisplay;
+  @tracked pagesToDisplay;
+  pagesToDisplayCount = 10;
+  itemsPerPage = 10;
+  pageSelected = 1;
+  orderBy;
+  pages;
+
+  get itemsStartIndex() {
+     return(this.pageSelected - 1) * this.itemsPerPage;
+  }
 
   constructor(owner, args) {
     super(owner, args);
+    this.orderBy = "country";
 
-    this.orderByField("country");
+    this.listToDisplay = this.getItemsToDisplay();
+    this.pages = [];
+    for (let i = 1; i <= Math.ceil(this.args.countries.length / this.itemsPerPage); i++) {
+      this.pages.push(i);
+    }
+
+    this.updatePagesToDisplay();
   }
 
   @action
-  orderBy(params) {
-    this.orderByField(params.target.value);
+  onPageSelected(page) {
+    this.pageSelected = page;
+    this.listToDisplay = this.getItemsToDisplay();
+    this.updatePagesToDisplay();
+  }
+
+  updatePagesToDisplay() {
+    const start = Math.max(0, this.pageSelected - this.pagesToDisplayCount / 2);
+    let nearbyPages = this.pages.slice(start, start + this.pagesToDisplayCount);
+    this.pagesToDisplay = nearbyPages.map(x => ({ value: x, active: x === this.pageSelected}));
+  }
+
+  @action
+  onChangedOrderCriteria(params) {
+    this.orderBy = params.target.value;
+    this.listToDisplay = this.getItemsToDisplay();
+  }
+
+  getItemsToDisplay() {
+    return this.orderByField(this.orderBy).slice(this.itemsStartIndex, this.itemsStartIndex + this.itemsPerPage);
   }
 
   orderByField(orderBy) {
@@ -24,6 +59,6 @@ export default class CountriesListComponent extends Component {
     else
       countries.sort((a, b) => b[orderBy] - a[orderBy]);
 
-    this.sortedList = countries;
+    return countries;
   }
 }
